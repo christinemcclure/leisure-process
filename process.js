cdc5d6ea6f6b53f10857a7d660ca43eeae58fd44
Christine McClure <camcclure@gmail.com>
2013-11-08 14:20:24 -0600

    basic flow control implemented from article example

--- process.js
+++ process.js
@@ -11,28 +11,32 @@ var moment = require('moment');
 moment().format();
 var logFile = './'+moment().format("YYYY-MM-DD")+'.log';
 var isbnFile = 'isbns-sample.txt';
+var isbnsToProcess=[];
 
 //  Create a log file of YYYY-MM-DD format. Delete any file if it exists
 
 
-function init(inputFile, process){
-  inputFile = isbnFile;
-  fs.exists(logFile, function (exists) {
-    if (exists){
-      fs.unlink(logFile, function (error) {
-        if (error) throw error;
-        if (debug) console.log('successfully deleted log file before processing: ' + logFile + '\n');
-      });
+// From http://blog.4psa.com/the-callback-syndrome-in-node-js/
+function series() {
+    var callbackSeries =  
+      [
+        init,
+        processISBNFile
+      ];
+ 
+    function next() {
+        var callback = callbackSeries.shift();
+        if (callback) {
+            callback(next);
+        }
+        else {
+            finish();
+        }
     }
-    logMsg('Processing started. Using Input file name: '+isbnFile);
-  } // end callback 
-  );// end function
-  console.log('\n'+ moment().format('YYYY-MM-DD HH:MM') + '\nProcessing started. Using ' + isbnFile + ' and writing messages to "'+ logFile +'".\n');
-}
+    next();
+};
 
-function process(){
-
-  
+ 
   //
 //  Open ISBN file
 //  For each line of the file:
@@ -50,11 +54,38 @@ function process(){
 //    - Remove ISBN from array.
 //
   
-}
 
-init(isbnFile);
+
+
+/// MAIN PROCESSING SECTION
+
+series();
+
+
+
+
 
 //// FUNCTIONS
+
+
+function finish() { console.log('Finished.'); }
+
+
+function init(callback){
+  fs.exists(logFile, function (exists) {
+    if (exists){
+      fs.unlink(logFile, function (error) {
+        if (error) throw error;
+        if (debug) console.log('successfully deleted log file before processing: ' + logFile + '\n');
+      });
+    }
+    logMsg('Processing started. Using Input file name: '+isbnFile);
+  });
+  console.log('\n'+ moment().format('YYYY-MM-DD HH:MM') + '\nProcessing started. Using ' + isbnFile + ' and writing messages to "'+ logFile +'".\n');
+  setTimeout(function() { callback(); }, 100);
+}
+
+//    processISBNFile(isbnFile);
 
 function logMsg(msg){
   var moment = require('moment');
@@ -65,3 +96,60 @@ function logMsg(msg){
   });
 }
 
+function processISBNFile(callback){
+    fs.readFile(isbnFile, 'utf8', function(error, fileData) {
+      // the data is passed to the callback in the second argument
+      if(error){
+        throw error;
+      }
+      //console.log('The file data is \n'+ fileData);
+      var isbns=fileData.split('\n');
+
+      for (var i=0; i< isbns.length; i++){
+        var isbnArr=isbns[i].split(' ');
+        var tempISBN = isbnArr[0].trim();// isbn will be first element in the array
+        if (debug) console.log('\n Element '+ i +' of the array= "'+tempISBN+'"\n');
+        var rt = checkISBN(tempISBN);
+        if (debug) console.log('\nrt from check ISBN='+rt+'\n');
+        if (rt==true){
+          rt = checkIfInArray(isbnsToProcess,tempISBN);
+          if (rt != true) isbnsToProcess.push(tempISBN); // only add if not already in array
+          if (debug) console.log('here is the array '+isbnsToProcess.toString());
+        }
+      }
+    });
+   setTimeout(function() { callback(); }, 100);
+ }
+ 
+ 
+function checkISBN(isbn) {
+// not a true validation, just ensuring that the split worked correctly
+  var exp, ret;
+  var debug = false; // set to true to do additional logging
+  if  (isbn.length === 10) {
+    exp = new RegExp(/\b(^\d{10}$|^\d{9}x)$\b/i); // ISBN-10 can be 10 digits, or 9 digits + x (checksum of 10)
+    if (debug) console.log('\n the length of '+isbn +' is '+ isbn.length +'\n');
+  }
+  else if (isbn.length === 13){
+    exp = new RegExp(/^978\d{10}$/); // ISBN-13 has different checksum logic. only digits
+  }
+  else {
+    console.log('\n"'+isbn+'" is a not valid isbn. \n');
+    return false; // quick check for length
+    //
+  }
+    ret=exp.test(isbn);
+    if (debug) console.log('\n regex returns '+ret +'\n');
+    return ret;
+}
+
+function checkIfInArray(arr, item){
+  var flag = false;
+  for (var i = 0; i < arr.length; i++){
+    if (arr[i]==item){
+      flag = true;
+      break;
+    }
+  }
+  return flag;
+}
