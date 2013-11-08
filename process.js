15aa6ab414ef877422be9a6199bc7929af96a178
Christine McClure <camcclure@gmail.com>
2013-11-08 13:18:40 -0600

    fixed comments and moved outline

--- process.js
+++ process.js
@@ -1,20 +1,3 @@
-//
-//  Open ISBN file
-//  For each line of the file:
-//    - Split line into an array by spaces
-//    - Trim leading/trailing spaces from array[0]
-//    - Verify array[0] is a valid ISBN. If not valid, write to log file and go to next line.
-//    - Check to see if this ISBN is in the "to be processed" array. If not, add it.
-//  For each element of the "to be processed" array:
-//    - Send API request to OCLC for that ISBN
-//    - Receive request. If error, write to log file
-//    - Take only title, author, summary, ISBNs, and Subjects from the results
-//    - Construct valid JSON object
-//    - Append to output file
-//    - Write success message to log file.
-//    - Remove ISBN from array.
-//
-
 // Modules
 var fs = require('fs'),
   xml2js = require('xml2js');
@@ -45,7 +28,27 @@ function process(){
       });
     }
     logMsg('Processing started. Using Input file name: '+isbnFile);
-  });
+    
+  } // end callback 
+  );// end function
+  
+  //
+//  Open ISBN file
+//  For each line of the file:
+//    - Split line into an array by spaces
+//    - Trim leading/trailing spaces from array[0]
+//    - Verify array[0] is a valid ISBN. If not valid, write to log file and go to next line.
+//    - Check to see if this ISBN is in the "to be processed" array. If not, add it.
+//  For each element of the "to be processed" array:
+//    - Send API request to OCLC for that ISBN
+//    - Receive request. If error, write to log file
+//    - Take only title, author, summary, ISBNs, and Subjects from the results
+//    - Construct valid JSON object
+//    - Append to output file
+//    - Write success message to log file.
+//    - Remove ISBN from array.
+//
+  
 }
 
 
