6fa27425c015305cefad2a347c48d351452ae50e
Christine McClure <camcclure@gmail.com>
2013-11-09 10:20:33 -0600

    working example of sync chain with parameters

--- test.js
+++ test.js
@@ -1,3 +1,44 @@
 var testKey = process.env.OCLC_DEV_KEY;
 
 console.log(testKey);
+series2();
+
+function series2() {
+    var callbackSeries =
+      [
+        func1, //  Create a log file of YYYY-MM-DD format. Delete any file if it exists
+        func2
+      ];
+
+    function next() {
+        var callback = callbackSeries.shift();
+        if (callback) {
+            callback(next);
+        }
+        else {
+            finish();
+        }
+    }
+    next();
+};
+
+function func1(callback, parm1){
+   parm1='asdfasf';
+  console.log(parm1);
+  setTimeout(function() {
+    callback();
+  }, 5000);
+}
+
+
+function func2(callback, parm2){
+   parm2='22222';
+  console.log(parm2);
+  setTimeout(function() {
+    callback();
+  }, 100);
+}
+
+function finish(){
+  console.log('done');
+}
